c207c624b605b846e9d2ef71b5a91750de8f11f2
hongdul <48382951+hongdul@users.noreply.github.com>
2025-02-14 15:43:12 +0900

    Time: 86 ms (26.61%), Space: 53.8 MB (48.36%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -1,6 +1,6 @@
 function reverse(x: number): number {
     let sign = x > 0 ? 1 : -1;
     let x_abs = Math.abs(x);
-    let num = parseInt(x_abs.toString().split('').reverse().join(''));
-    return num * sign > 2**31 - 1 || num * sign < -(2**31) ? 0 : num * sign;
+    let num = parseInt(x_abs.toString().split('').reverse().join('')) * sign;
+    return num > 2**31 - 1 || num  < -(2**31) ? 0 : num;
 };
