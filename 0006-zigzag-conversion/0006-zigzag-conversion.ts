b2a5792db522b4f8e5360d076380b4b7721da881
hongdul <48382951+hongdul@users.noreply.github.com>
2025-02-15 14:23:49 +0900

    Time: 15 ms (42.14%), Space: 60.2 MB (8.5%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.ts
@@ -0,0 +1,18 @@
+function convert(s: string, numRows: number): string {
+    let int_arr: string[][] = [];
+    for (let i = 0; i < numRows; i++) {
+        int_arr.push([]);
+    }
+
+    while (s) {
+        for (let i = 0; i < numRows && s; i++) {
+            int_arr[i].push(s[0]);
+            s = s.slice(1);
+        }
+        for (let i = numRows - 2; i > 0 && s; i--) {
+            int_arr[i].push(s[0]);
+            s = s.slice(1);
+        }
+    }
+    return int_arr.map((arr) => arr.join('')).join('');
+};
