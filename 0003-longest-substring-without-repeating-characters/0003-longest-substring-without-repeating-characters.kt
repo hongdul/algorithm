65776e349cbf41674486d25489c6937200f4327a
hongdul <48382951+hongdul@users.noreply.github.com>
2025-03-02 01:04:40 +0900

    Time: 17 ms (34.52%), Space: 39.3 MB (27.15%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var word = ""
+        var longestLength = 0
+
+        for (c in s) {
+            val charIndex = word.indexOf(c)
+            if (charIndex != -1) {
+                if (word.length > longestLength) {
+                    longestLength = word.length
+                }
+                word = word.substring(charIndex + 1)
+            }
+            word += c
+        }
+
+        if (word.length > longestLength) {
+            longestLength = word.length
+        }
+
+        return if (longestLength == 0) s.length else longestLength
+    }
+}
