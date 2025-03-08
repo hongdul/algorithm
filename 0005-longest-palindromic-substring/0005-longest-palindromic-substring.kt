ef590134a6446c782040bc3128ec869b52c33beb
hongdul <48382951+hongdul@users.noreply.github.com>
2025-03-08 13:45:22 +0900

    Time: 1751 ms (5.28%), Space: 53.1 MB (5.03%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun longestPalindrome(s: String): String {
+        var longest = ""
+
+        for ((i, v) in s.withIndex()) {
+            val charMap = s.mapIndexedNotNull { index, c -> if (c == v) index else null }
+                .filter { it > i }
+            if (charMap.isEmpty()) {
+                longest = longest.ifEmpty { v.toString() }
+            }
+            
+            for (j in charMap) {
+                val sub = s.substring(i, j + 1)
+                if (sub == sub.reversed()) {
+                    longest = if (sub.length > longest.length) sub else longest
+                }
+            }
+        }
+        return longest
+    }
+}
