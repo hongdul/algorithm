9e0f743351d382dbd78f44c72053912b621fd495
hongdul <48382951+hongdul@users.noreply.github.com>
2025-03-17 21:15:07 +0900

    Time: 12 ms (79.59%), Space: 41.7 MB (23.11%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.kt
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.kt
@@ -1,27 +1,28 @@
 class Solution {
     fun longestPalindrome(s: String): String {
-        if (s.length <= 1) return s
+    if (s.isEmpty()) return ""
 
-        var longest = ""
+    var start = 0
+    var maxLength = 0
 
-        fun checkPalindrome(left: Int, right: Int) {
-            var l = left
-            var r = right
-            while (l >= 0 && r < s.length) {
-                if (s[l] == s[r]) {
-                    l--
-                    r++
-                } else break
-            }
-            if (r - l - 1 > longest.length) {
-                longest = s.substring(l + 1, r)
-            }
+    fun checkPalindrome(left: Int, right: Int) {
+        var l = left
+        var r = right
+        while (l >= 0 && r < s.length && s[l] == s[r]) {
+            l--
+            r++
         }
-        for (i in s.indices) {
-            checkPalindrome(i, i)
-            checkPalindrome(i, i + 1)
+        if (r - l - 1 > maxLength) {
+            start = l + 1
+            maxLength = r - l - 1
         }
-
-        return longest
     }
+
+    for (i in s.indices) {
+        checkPalindrome(i, i)   
+        checkPalindrome(i, i + 1)
+    }
+
+    return s.substring(start, start + maxLength)
+}
 }
