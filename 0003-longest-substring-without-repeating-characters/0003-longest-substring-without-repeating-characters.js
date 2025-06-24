cf3f2338b354db5c61b49c26e94b6a72687d3bee
hongdul <48382951+hongdul@users.noreply.github.com>
2025-06-24 22:31:54 +0900

    Time: 4 ms (89.7%), Space: 60.3 MB (21.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -18,3 +18,23 @@ var lengthOfLongestSubstring = function(s) {
 
     return maxLen;
 };
+
+/**
+    - Set + 투 포인터 방식 -
+    var lengthOfLongestSubstring = function(s) {
+        let set = new Set();
+        let maxLen = 0;
+        let left = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            while (set.has(s[right])) {
+                set.delete(s[left]);
+                left++;
+            }
+            set.add(s[right]);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    };
+*/
