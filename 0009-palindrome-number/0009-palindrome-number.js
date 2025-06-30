41a99e38a091a33761a888779d59f2ee98cc2bf9
hongdul <48382951+hongdul@users.noreply.github.com>
2025-06-30 15:37:39 +0900

    Time: 6 ms (70.47%), Space: 65.1 MB (27.02%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,26 +6,27 @@ var isPalindrome = function(x) {
     if (x < 0) return false;
 
     x = x.toString();
-    const len = x.length;
-    const isOdd = len % 2 != 0;
+    return x == x.split('').reverse().join('');
+    // const len = x.length;
+    // const isOdd = len % 2 != 0;
 
-    if (isOdd) return calcPalindrome(Math.floor(len / 2), isOdd);
-    return calcPalindrome(len / 2 - 1, isOdd);
+    // if (isOdd) return calcPalindrome(Math.floor(len / 2), isOdd);
+    // return calcPalindrome(len / 2 - 1, isOdd);
 
-    function calcPalindrome(mid, isOdd) {
-        if (isOdd) {
-            for (let i = 1; i <= mid; i++) {
-                if (x[mid - i] == x[mid + i]) continue;
-                return false;
-            }
-            return true;
-        }
+    // function calcPalindrome(mid, isOdd) {
+    //     if (isOdd) {
+    //         for (let i = 1; i <= mid; i++) {
+    //             if (x[mid - i] == x[mid + i]) continue;
+    //             return false;
+    //         }
+    //         return true;
+    //     }
 
-        let mid2 = mid + 1;
-        for (let i = 0; i <= mid; i++) {
-            if (x[mid - i] == x[mid2 + i]) continue;
-            return false;
-        }
-        return true;
-    }
+    //     let mid2 = mid + 1;
+    //     for (let i = 0; i <= mid; i++) {
+    //         if (x[mid - i] == x[mid2 + i]) continue;
+    //         return false;
+    //     }
+    //     return true;
+    // }
 };
