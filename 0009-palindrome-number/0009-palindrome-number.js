4b37cbc12ab383c4f2441ab5d2bd112e12e79eb8
hongdul <48382951+hongdul@users.noreply.github.com>
2025-06-30 15:34:56 +0900

    Time: 9 ms (41.62%), Space: 65 MB (36.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+
+    x = x.toString();
+    const len = x.length;
+    const isOdd = len % 2 != 0;
+
+    if (isOdd) return calcPalindrome(Math.floor(len / 2), isOdd);
+    return calcPalindrome(len / 2 - 1, isOdd);
+
+    function calcPalindrome(mid, isOdd) {
+        if (isOdd) {
+            for (let i = 1; i <= mid; i++) {
+                if (x[mid - i] == x[mid + i]) continue;
+                return false;
+            }
+            return true;
+        }
+
+        let mid2 = mid + 1;
+        for (let i = 0; i <= mid; i++) {
+            if (x[mid - i] == x[mid2 + i]) continue;
+            return false;
+        }
+        return true;
+    }
+};
