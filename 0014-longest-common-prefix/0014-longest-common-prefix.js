98e6b291e12765c65bff467909bf183b7fa61cac
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-04 17:33:23 +0900

    Time: 2 ms (41.88%), Space: 56 MB (22.46%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let str = strs[0];
+    let answer = '';
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        for (let n = 0; n < strs.length; n++) {
+            if (strs[n][i] == char) continue;
+            return answer;
+        }
+        answer += char;
+    }
+    
+    return answer;
+};
