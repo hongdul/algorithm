24276bd2f6c7b5e59916f55a718c6c2cd126ac05
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-04 15:31:33 +0900

    Time: 4 ms (78.02%), Space: 62.5 MB (43.15%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -28,9 +28,25 @@ var intToRoman = function(num) {
         else if(cur >= 4) mid = fiftyChar;
 
         answer += prefix + mid + suffix;
-        
+
         num %= Math.pow(10, i);
     }
 
     return answer;
 };
+
+/** 하드코딩 방식
+var intToRoman = function(num) {
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+
+    let res = '';
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            num -= values[i];
+            res += symbols[i];
+        }
+    }
+    return res;
+};
+*/
