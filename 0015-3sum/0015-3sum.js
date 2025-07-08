025adbffddcadae2a4a231338588aaf5825188e2
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-08 13:31:40 +0900

    Time: 30 ms (92.63%), Space: 71.3 MB (90.95%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,37 +3,6 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    // nums.sort((a, b) => a - b);
-    // const result = [];
-
-    // for (let i = 0; i < nums.length - 2; i++) {
-    //     // 중복된 숫자는 스킵
-    //     if (i > 0 && nums[i] === nums[i - 1]) continue;
-
-    //     let left = i + 1;
-    //     let right = nums.length - 1;
-
-    //     while (left < right) {
-    //         const sum = nums[i] + nums[left] + nums[right];
-
-    //         if (sum === 0) {
-    //             result.push([nums[i], nums[left], nums[right]]);
-
-    //             // 중복 제거
-    //             while (left < right && nums[left] === nums[left + 1]) left++;
-    //             while (left < right && nums[right] === nums[right - 1]) right--;
-
-    //             left++;
-    //             right--;
-    //         } else if (sum < 0) {
-    //             left++;
-    //         } else {
-    //             right--;
-    //         }
-    //     }
-    // }
-
-    // return result;
     let answer = [];
     nums.sort((a, b) => a - b);
 
