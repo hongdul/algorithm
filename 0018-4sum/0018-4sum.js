8d20f392e96edefd7a16c65b22891b65634b2fce
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-09 14:53:42 +0900

    Time: 20 ms (32.19%), Space: 56.7 MB (76.47%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -20,16 +20,16 @@ var fourSum = function(nums, target) {
                 
                 if (sum == target) {
                     answer.push([nums[start], nums[m],  nums[n], nums[end]]);
-                    do { m++ } while (nums[m] == nums[m - 1])
-                    do { n-- } while (nums[n] == nums[n + 1])
+                    do { m++ } while (nums[m] === nums[m - 1])
+                    do { n-- } while (nums[n] === nums[n + 1])
                 } else if (sum < target) {
                     m++;
                 } else n--;
             }
 
-            do { end-- } while (nums[end] == nums[end + 1])
+            do { end-- } while (nums[end] === nums[end + 1])
         }
-        do { start++ } while (nums[start] == nums[start - 1])
+        do { start++ } while (nums[start] === nums[start - 1])
         end = nums.length - 1;
     }
 
