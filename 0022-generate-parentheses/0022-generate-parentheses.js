8e64b8e29252f9add9714378e32fc536e35d559e
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-09 17:54:04 +0900

    Time: 1 ms (85.51%), Space: 54.5 MB (46.61%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,24 +3,41 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    let answer = [];
+// gpt - 백트래킹
+    const answer = [];
 
-    for (let i = 1; i < 2 ** (2 * n - 1); i++) {
-        const binStr = i.toString(2).padStart(2 * n, '0');
-
-        const ones = binStr.split('1').length - 1;
-        if (ones !== n) continue;
-        
-        let zeroCount = 0;
-        for (let c = 0; c < binStr.length; c++) {
-            if (binStr[c] === '0') zeroCount++;
-            else zeroCount--;
-            if (zeroCount < 0) break;
+    const backtrack = (str, open, close) => {
+        if (str.length === 2 * n) {
+            answer.push(str);
+            return;
         }
-        if (zeroCount !== 0) continue;
 
-        answer.push(binStr.replaceAll('0', '(').replaceAll('1', ')'));
-    }
+        if (open < n) backtrack(str + '(', open + 1, close);
+        if (close < open) backtrack(str + ')', open, close + 1);
+    };
 
+    backtrack("", 0, 0);
     return answer;
+
+// 내 풀이
+    // let answer = [];
+
+    // for (let i = 1; i < 2 ** (2 * n - 1); i++) {
+    //     const binStr = i.toString(2).padStart(2 * n, '0');
+
+    //     const ones = binStr.split('1').length - 1;
+    //     if (ones !== n) continue;
+        
+    //     let zeroCount = 0;
+    //     for (let c = 0; c < binStr.length; c++) {
+    //         if (binStr[c] === '0') zeroCount++;
+    //         else zeroCount--;
+    //         if (zeroCount < 0) break;
+    //     }
+    //     if (zeroCount !== 0) continue;
+
+    //     answer.push(binStr.replaceAll('0', '(').replaceAll('1', ')'));
+    // }
+
+    // return answer;
 };
