e78b040abcf6917dfe8d7eb4c6666a2eeaa9fc42
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-09 11:22:21 +0900

    Time: 0 ms (100%), Space: 54 MB (57.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,31 +11,43 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let count = 1;
-    let nodeLen = 30;
+    const dummy = new ListNode(0, head);
 
-    recur(head, count);
-    
-    function recur(node, count) {
-        let a = count;
-
-        if(node.next) {
-            count++;
-            a = recur(node.next, count);
-        }
-        else nodeLen = count;
-
-        if (nodeLen == n) {
-            head = node.next;
-            return;
-        }
-
-        if (a == nodeLen - n) {
-            node.next = node.next.next;
-            return;
-        }
-        else return a - 1;
+    function recurse(node) {
+        if (!node) return 0;
+        const pos = recurse(node.next) + 1;
+        if (pos === n + 1) node.next = node.next.next;
+        return pos;
     }
 
-    return head;
+    recurse(dummy);
+    return dummy.next;
+    
+    // let count = 1;
+    // let nodeLen = 30;
+
+    // recur(head, count);
+    
+    // function recur(node, count) {
+    //     let a = count;
+
+    //     if(node.next) {
+    //         count++;
+    //         a = recur(node.next, count);
+    //     }
+    //     else nodeLen = count;
+
+    //     if (nodeLen == n) {
+    //         head = node.next;
+    //         return;
+    //     }
+
+    //     if (a == nodeLen - n) {
+    //         node.next = node.next.next;
+    //         return;
+    //     }
+    //     else return a - 1;
+    // }
+
+    // return head;
 };
