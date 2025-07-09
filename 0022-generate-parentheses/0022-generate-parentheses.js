3df4dd22cc653a17bfe33e44e70eeab27387b939
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-09 17:24:08 +0900

    Time: 40 ms (5%), Space: 60.6 MB (5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let answer = [];
+
+    for (let i = 0; i < 2 ** (2 * n); i++) {
+        const binStr = i.toString(2).padStart(2 * n, '0');
+
+        const ones = binStr.split('1').length - 1;
+        if (ones !== n) continue;
+        
+        let zeroCount = 0;
+        for (let c = 0; c < binStr.length; c++) {
+            if (binStr[c] === '0') zeroCount++;
+            else zeroCount--;
+            if (zeroCount < 0) break;
+        }
+        if (zeroCount !== 0) continue;
+
+        answer.push(binStr.replaceAll('0', '(').replaceAll('1', ')'));
+    }
+
+    return answer;
+};
