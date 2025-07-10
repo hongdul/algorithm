7c257b4ae550c76b575e543f784d48b091163bd4
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-10 18:50:55 +0900

    Time: 0 ms (100%), Space: 56.7 MB (81.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,20 +5,15 @@
 var removeDuplicates = function(nums) {
     if (nums.length === 0) return 0;
 
-    let answer = 1;
-    let cur = nums[0];
-    let i = 1;
-    let temp = i;
-
-    while (temp < nums.length) {
-        while (cur === nums[temp]) temp++;
-        if (temp >= nums.length) break;
-        nums[i] = nums[temp];
-        cur = nums[temp];
-        temp++;
-        i++;
-        answer++;
+    let i = 0;
+    let j = 0;
+    while (j < nums.length) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+        j++;
     }
 
-    return answer;
+    return i + 1;
 };
