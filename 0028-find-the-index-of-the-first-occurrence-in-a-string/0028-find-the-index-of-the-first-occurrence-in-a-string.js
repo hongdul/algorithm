4a54f09236f118a86764e0a41a2c887bcfa31628
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-11 16:33:56 +0900

    Time: 1 ms (17.94%), Space: 53.3 MB (81.21%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -24,4 +24,54 @@ var strStr = function(haystack, needle) {
         }   
     }
     return i - needle.length;
+    
+// gpt - KMP 알고리즘 
+    // if (needle.length === 0) return 0;
+
+    // // 1. LPS 배열 생성
+    // const lps = buildLPS(needle);
+
+    // let i = 0; // haystack index
+    // let j = 0; // needle index
+
+    // while (i < haystack.length) {
+    //     if (haystack[i] === needle[j]) {
+    //         i++;
+    //         j++;
+    //         if (j === needle.length) {
+    //             return i - j; // 전체 일치!
+    //         }
+    //     } else {
+    //         if (j > 0) {
+    //             j = lps[j - 1]; // jump!
+    //         } else {
+    //             i++;
+    //         }
+    //     }
+    // }
+
+    // return -1; // 일치하지 않음
+
+    // // LPS 함수 정의
+    // function buildLPS(pattern) {
+    //     const lps = new Array(pattern.length).fill(0);
+    //     let len = 0;
+    //     let i = 1;
+
+    //     while (i < pattern.length) {
+    //         if (pattern[i] === pattern[len]) {
+    //             len++;
+    //             lps[i] = len;
+    //             i++;
+    //         } else {
+    //             if (len > 0) {
+    //                 len = lps[len - 1]; // 이전 일치 정보로 돌아감
+    //             } else {
+    //                 lps[i] = 0;
+    //                 i++;
+    //             }
+    //         }
+    //     }
+    //     return lps;
+    // }
 };
