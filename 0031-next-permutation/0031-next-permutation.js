da4fab984e579a594e024d5a13307b4d238fbf5e
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-14 17:59:03 +0900

    Time: 2 ms (10.48%), Space: 57.6 MB (5.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -22,3 +22,30 @@ var nextPermutation = function(nums) {
         }
     }
 };
+
+// 최적화
+    // let i = nums.length - 1;
+
+    // // 1. 뒤에서부터 감소하지 않는 최초 구간 찾기
+    // while (i > 0 && nums[i - 1] >= nums[i]) i--;
+
+    // if (i === 0) {
+    //     // 전체 내림차순 → 처음 순열로 리셋
+    //     nums.reverse();
+    //     return;
+    // }
+
+    // // 2. nums[i - 1]보다 큰 수 중 가장 작은 수를 뒤에서부터 찾기
+    // let j = nums.length - 1;
+    // while (nums[j] <= nums[i - 1]) j--;
+
+    // // 3. swap
+    // [nums[i - 1], nums[j]] = [nums[j], nums[i - 1]];
+
+    // // 4. 뒷부분 오름차순 정렬 (원래 내림차순이므로 reverse로 가능)
+    // let left = i, right = nums.length - 1;
+    // while (left < right) {
+    //     [nums[left], nums[right]] = [nums[right], nums[left]];
+    //     left++;
+    //     right--;
+    // }
