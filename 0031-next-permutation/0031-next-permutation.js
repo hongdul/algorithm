2ffc2c978f3ff895d40dfbbff8801a91e27aa483
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-14 16:54:51 +0900

    Time: 1 ms (24.88%), Space: 56.9 MB (5.93%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -8,16 +8,13 @@ var nextPermutation = function(nums) {
     for (let i = nums.length - 1; i > 0; i--) {
         numsMap.set(nums[i], i);
         if (nums[i] <= nums[i - 1]) continue;
-        console.log(i, nums[i], numsMap);
+        
         let numbers = numsMap.keys();
-        console.log('numbers', numbers);
         let minNum = Math.min(...numbers.filter((n) => n > nums[i - 1]));
-        console.log('minNum', minNum);
+
         let temp = nums[i - 1];
-        console.log('temp', temp);
         nums[i - 1] = minNum;
         nums[numsMap.get(minNum)] = temp;
-        console.log(nums);
 
         let sortedNums = nums.slice(i).sort((a, b) => a - b);
         
