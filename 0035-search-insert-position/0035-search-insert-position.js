a526e206c8e106cfe0dd053eba44c749c928e56b
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-18 13:07:44 +0900

    Time: 0 ms (100%), Space: 54.1 MB (34.22%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,23 +4,19 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if (target < nums[0]) return 0;
-    if (target > nums[nums.length - 1]) return nums.length;
-
     let left = 0;
     let right = nums.length - 1;
-    let i = Math.floor((left + right) / 2);
+    
 
     let maxCount = Math.floor(Math.log2(nums.length));
     let count = 0;
 
     while (left <= right) {
+        let i = Math.floor((left + right) / 2);
         if (nums[i] > target) {
             right = i - 1;
-            i = Math.floor((left + right) / 2);
         } else if (nums[i] < target) {
             left = i + 1;
-            i = Math.floor((left + right) / 2);
         } else {
             return i;
         }
