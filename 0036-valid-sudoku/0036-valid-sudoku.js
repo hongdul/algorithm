e6055131014d1145ff65ce5ab3ed3fe3db791690
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-18 16:29:08 +0900

    Time: 117 ms (5.05%), Space: 64 MB (5.02%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    let numMap = new Map();
+    let key;
+
+    for (let i = 0; i < board.length; i++) {
+        let line = board[i];
+        console.log(numMap);
+        for (let n = 0; n < line.length; n++) {
+            if (line[n] === '.') continue;
+
+            // 가로
+            key = `row${i}_${line[n]}`;
+            console.log(key);
+            if (numMap.has(key)) return false;
+            numMap.set(key, true);
+            
+            // 세로
+            key = `col${n}_${line[n]}`;
+            console.log(key);
+            if (numMap.has(key)) return false;
+            numMap.set(key, true);
+
+            // 3x3
+            key = `box${Math.floor(n / 3)}${Math.floor(i / 3)}_${line[n]}`;
+            console.log(key);
+            if (numMap.has(key)) return false;
+            numMap.set(key, true);
+        }
+    }
+
+    return true;
+};
