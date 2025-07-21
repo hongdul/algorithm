843b2bc59db4147e24f4f4357e370a1cf6c9d5f4
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-21 13:38:43 +0900

    Time: 8 ms (20.85%), Space: 64.5 MB (6.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -7,13 +7,12 @@ var combinationSum = function(candidates, target) {
     let answer = [];
 
     for (let l = 0; l < candidates.length; l++) {
-        backtrack([candidates[l]], l);
+        backtrack([candidates[l]], l, candidates[l]);
     }
 
     return answer;
     
-    function backtrack(numbers, l) {
-        let sum = getSum(numbers);
+    function backtrack(numbers, l, sum) {
         if (sum > target) return;
         if (sum === target) {
             answer.push(numbers);
@@ -21,13 +20,7 @@ var combinationSum = function(candidates, target) {
         }
 
         for(let i = l; i < candidates.length; i++) {
-            if (candidates[i] > target) continue;
-            let temp = [...numbers, candidates[i]];
-            backtrack(temp, i);
+            backtrack([...numbers, candidates[i]], i, sum + candidates[i]);
         }
-    }
-    function getSum(numbers) {
-        console.log(numbers);
-        return numbers.reduce((a, b) => a + b, 0);
     }
 };
