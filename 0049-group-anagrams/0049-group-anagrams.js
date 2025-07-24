b6940aa7cb1c8f54f0231fd7737132f29678c3d4
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-24 11:14:29 +0900

    Time: 23 ms (90.74%), Space: 66.7 MB (81.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,19 +4,14 @@
  */
 var groupAnagrams = function(strs) {
     let exists = new Map();
-    let count = 0;
-    let answer = [];
     
     for (let i = 0; i < strs.length; i++) {
         let str = strs[i].split("").sort().join("");
-        let index = exists.get(str);
-        if (index === undefined) {
-            exists.set(str, count);
-            count++;
-            answer.push([strs[i]]);
+        if (!exists.has(str)) {
+            exists.set(str, [strs[i]]);
         } else {
-            answer[index].push(strs[i]);
+            exists.get(str).push(strs[i]);
         }
     }
-    return answer;
+    return Array.from(exists.values());
 };
