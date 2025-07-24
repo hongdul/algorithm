722c18a505c02304a06518e82d6e4647c4d2d541
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-24 10:33:34 +0900

    Time: 0 ms (100%), Space: 55.6 MB (15.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -19,4 +19,19 @@ var rotate = function(matrix) {
         }
         matrix[i] = temp;
     }
+
+// gpt - 대각선 원소 교환 후 각 행 뒤집기
+// const n = matrix.length;
+
+// // 1. Transpose
+// for (let i = 0; i < n; i++) {
+//     for (let j = i + 1; j < n; j++) {
+//         [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+//     }
+// }
+
+// // 2. Reverse each row
+// for (let row of matrix) {
+//     row.reverse();
+// }
 };
