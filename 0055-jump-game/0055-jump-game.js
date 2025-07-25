08530ab54e1d59038361db82264cffc04a6b31ca
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-25 20:28:09 +0900

    Time: 1 ms (84.65%), Space: 58.9 MB (52.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,23 +3,11 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    if (nums.length === 1) return true;
-    let answer = false;
-    let falseMap = new Map();
-    btr(0);
+    let max = 0;
 
-    return answer;
-
-    function btr(i) {
-        if (falseMap.has(i)) return;
-        for (let j = nums[i]; j > 0; j--) {
-            if (i + j >= nums.length - 1) {
-                answer = true;
-                return;
-            }
-            btr(i + j);
-            if (answer === true) return;
-            else falseMap.set(i, false);
-        }
+    for (let i = 0; i < nums.length; i++) {
+        if (i > max) return false;
+        max = max > i + nums[i] ? max : i + nums[i];
     }
+    return true;
 };
