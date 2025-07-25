1aee6122fcc6e2442609a41d58712bf423f262de
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-25 13:17:08 +0900

    Time: 40 ms (5.17%), Space: 62.6 MB (8.07%) - LeetHub

--- 0051-n-queens/0051-n-queens.js
+++ 0051-n-queens/0051-n-queens.js
@@ -21,9 +21,6 @@ var solveNQueens = function(n) {
             .split("")
             .map((char, idx) => (char === "." ? idx : -1))
             .filter(idx => idx !== -1);
-        console.log(map, row);
-        console.log(cur);
-        console.log(posIndex);
         for (let i = 0; i < posIndex.length; i++) {
             let temp = new Map(map);
             cur = cur.split("").map((value, idx) => {
@@ -31,23 +28,18 @@ var solveNQueens = function(n) {
                 else return '.';
             }).join("");
             temp.set(row, cur);
-            console.log('temp: ', temp);
             block(temp, row, posIndex[i]);
-            console.log('temp2: ', temp);
             btr(temp, row + 1);
         }
     }
     function block(map, row, index) {
         for (let i = 1; i + row < n; i++) {
             let nextRow = map.get(row + i).split("");
-            console.log('in block: ', i, nextRow);
             for (let r = 0; r < nextRow.length; r++) {
-                console.log('r: ', i, index, r);
                 if (r === index - i || r === index + i || r === index) {
                     nextRow[r] = 'X';
                 }
             }
-            console.log('nextRow: ', nextRow);
             map.set(row + i, nextRow.join(""));
         }
     }
