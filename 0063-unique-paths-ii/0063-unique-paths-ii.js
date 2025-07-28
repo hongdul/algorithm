3eb973b32c672b8c7c7e413639e0aadcfaba14ef
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-28 20:34:28 +0900

    Time: 1 ms (61.02%), Space: 56.4 MB (8.68%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.js
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -3,26 +3,21 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-    let m = obstacleGrid.length;
-    let n = obstacleGrid[0].length;
-    if (obstacleGrid[0][0] === 1 || obstacleGrid[m - 1][n - 1] === 1) return 0;
-    let dp = Array.from({ length: m }, () => Array(n).fill(1));
-    
+    const m = obstacleGrid.length;
+    const n = obstacleGrid[0].length;
+
+    const dp = Array.from({ length: m }, () => Array(n).fill(0));
+    if (obstacleGrid[0][0] === 1) return 0;
+    dp[0][0] = 1;
+
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (obstacleGrid[i][j] === 1) {
                 dp[i][j] = 0;
                 continue;
             }
-            if (i === 0 && j > 0) {
-                dp[i][j] = dp[i][j - 1];
-            } else if (i > 0 && j === 0) {
-                dp[i][j] = dp[i - 1][j];
-            } else if (i === 0 && j === 0) {
-                continue;
-            } else {
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-            }
+            if (i > 0) dp[i][j] += dp[i - 1][j];
+            if (j > 0) dp[i][j] += dp[i][j - 1];
         }
     }
 
