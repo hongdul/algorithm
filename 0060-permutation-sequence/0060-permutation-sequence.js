41141c014e5a4efe639cc5860b789b96d0bdd7b3
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-28 14:14:08 +0900

    Time: 1 ms (50.5%), Space: 53.3 MB (79.5%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.js
+++ 0060-permutation-sequence/0060-permutation-sequence.js
@@ -4,33 +4,17 @@
  * @return {string}
  */
 var getPermutation = function(n, k) {
-    let fac = 1;
+    let factorial = [1];
     for (let i = 1; i <= n; i++) {
-        fac *= i;
-    }
-    let indexArr = Array.from({ length: n }, () => 0);
-
-    let temp = fac;
-    for (let i = n; i >= 1; i--) {
-        temp = Math.floor(temp / i);
-        let quotient = 0;
-        if (k >= temp) {
-            for (let l = 1; l <= i; l++) {
-                quotient += temp;
-                if (k <= quotient) {
-                    indexArr[n - i] = l - 1;
-                    quotient -= temp;
-                    break;
-                }
-            }
-        }
-        k -= quotient;
+        factorial[i] = factorial[i - 1] * i;
     }
     let numbers = Array.from({ length: n }, (_, i) => i + 1);
     let answer = "";
-
-    for (let i = 0; i < indexArr.length; i++) {
-        answer += numbers.splice(indexArr[i], 1);
+    k--;
+    for (let i = n; i >= 1; i--) {
+        let index = Math.floor(k / factorial[i - 1]);
+        answer += numbers.splice(index, 1);
+        k %= factorial[i - 1];
     }
 
     return answer;
