f1a989d96146bcbdc503001cd656d1a8ca12d951
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-28 14:01:14 +0900

    Time: 20 ms (19.5%), Space: 57.5 MB (14%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function(n, k) {
+    let fac = 1;
+    for (let i = 1; i <= n; i++) {
+        fac *= i;
+    }
+    let indexArr = Array.from({ length: n }, () => 0);
+    console.log(indexArr);
+
+    let temp = fac;
+    for (let i = n; i >= 1; i--) {
+        temp = Math.floor(temp / i);
+
+        console.log(temp, k);
+        let quotient = 0;
+        if (k >= temp) {
+            for (let l = 1; l <= i; l++) {
+                quotient += temp;
+                if (k <= quotient) {
+                    indexArr[n - i] = l - 1;
+                    quotient -= temp;
+                    break;
+                }
+            }
+        }
+        k -= quotient;
+    }
+    console.log(indexArr);
+    let numbers = Array.from({ length: n }, (_, i) => i + 1);
+    console.log(numbers);
+    let answer = "";
+    for (let i = 0; i < indexArr.length; i++) {
+        answer += numbers.splice(indexArr[i], 1);
+    }
+    return answer;
+};
