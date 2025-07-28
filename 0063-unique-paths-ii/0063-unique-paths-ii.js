76fd38624e9aa4a37703f1056d7477d994fa32f4
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-28 20:32:29 +0900

    Time: 1 ms (61.02%), Space: 54.5 MB (39.67%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.js
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -5,6 +5,7 @@
 var uniquePathsWithObstacles = function(obstacleGrid) {
     let m = obstacleGrid.length;
     let n = obstacleGrid[0].length;
+    if (obstacleGrid[0][0] === 1 || obstacleGrid[m - 1][n - 1] === 1) return 0;
     let dp = Array.from({ length: m }, () => Array(n).fill(1));
     
     for (let i = 0; i < m; i++) {
@@ -18,7 +19,7 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
             } else if (i > 0 && j === 0) {
                 dp[i][j] = dp[i - 1][j];
             } else if (i === 0 && j === 0) {
-                if (obstacleGrid[i][j] === 1) return 0;
+                continue;
             } else {
                 dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
