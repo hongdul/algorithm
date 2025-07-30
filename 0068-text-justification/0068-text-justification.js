041ebea14f76b698372054260a62e3a83c31cfa3
hongdul <48382951+hongdul@users.noreply.github.com>
2025-07-30 17:08:07 +0900

    Time: 0 ms (100%), Space: 54.1 MB (19.84%) - LeetHub

--- 0068-text-justification/0068-text-justification.js
+++ 0068-text-justification/0068-text-justification.js
@@ -12,51 +12,36 @@ var fullJustify = function(words, maxWidth) {
     let tempLen = 0;
     while (i < words.length) {
         let word = words[i];
-        
-        if (i === 0 || tempLen + word.length + 1 > maxWidth) {
-            if (tempWordArr.length !== 0) answer.push(makeStr(tempWordArr));
-            tempWordArr = [word];
-            tempLen = word.length;
-        } else {
-            tempWordArr.push(word);
-            tempLen += word.length + 1;
+        if (tempLen + tempWordArr.length + word.length > maxWidth) {
+            answer.push(makeStr(tempWordArr));
+            tempWordArr = [];
+            tempLen = 0;
         }
-        if (i === words.length - 1) {
-            answer.push(tempWordArr.join(" ").padEnd(maxWidth, " "));
-        }
+        tempWordArr.push(word);
+        tempLen += word.length;
+
         i++;
     }
+    answer.push(tempWordArr.join(" ").padEnd(maxWidth, " "));
 
     return answer;
 
     function makeStr(wordArr) {
-        if (wordArr.length === 1) {
-            return wordArr[0].padEnd(maxWidth, " ");
-        } else {
-            let len = wordArr.reduce((acc, cur) => {
-                acc += cur.length;
-                return acc;
-            }, 0)
-            
-            let extraSpaces = (maxWidth - len) % (wordArr.length - 1);
-            let spaces = wordArr.reduce((acc, cur, idx) => {
-                if (idx === 0) {
-                    acc.push("");
-                } else if (extraSpaces > 0) {
-                    acc.push(" ".repeat(1 + Math.floor((maxWidth - len) / (wordArr.length - 1))));
-                    extraSpaces--;
-                } else {
-                    acc.push(" ".repeat(Math.floor((maxWidth - len) / (wordArr.length - 1))));
-                }
-                return acc;
-            }, []);
-            return wordArr.reduce((acc, cur, idx) => {
-                if (idx === 0) return cur;
-                else {
-                    acc += spaces[idx] + cur;
-                    return acc;
-                }
-            }, "");
+        if (wordArr.length === 1) return wordArr[0].padEnd(maxWidth, " ");
+        
+        let totalChars = wordArr.reduce((acc, cur) => acc + cur.length, 0);
+        let totalSpaces = maxWidth - totalChars;
+        let gaps = wordArr.length - 1;
+        let evenSpace = Math.floor(totalSpaces / gaps);
+        let extraSpace = totalSpaces % gaps;
+
+        let line = "";
+        for (let i = 0; i < wordArr.length; i++) {
+            line += wordArr[i];
+            if (i < gaps) {
+                line += " ".repeat(evenSpace + (i < extraSpace ? 1 : 0));
+            }
         }
+        return line;
     }
 };
