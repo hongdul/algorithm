3a2c82a7e4d18f5002753127cbcdca13e867580b
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-01 11:23:49 +0900

    Time: 67 ms (5.04%), Space: 60.8 MB (6.34%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let answer = "";
+    let pathArr = path.split("/").filter((it) => it !== '');
+    console.log(pathArr);
+    let i = 0;
+    while (i < pathArr.length) {
+        switch (pathArr[i]) {
+            case ".": 
+                pathArr.splice(i, 1);
+                break;
+            case "..":
+                if (i !== 0) {
+                    pathArr.splice(i - 1, 2);
+                    i--;
+                } else {
+                    pathArr.splice(i, 1);
+                }
+                break;
+            default: 
+                i++;
+                break;
+        }
+    }
+    return pathArr.reduce((acc, cur) => {
+        console.log(cur);
+        acc += `/${cur}`;
+        return acc;
+    }, "") || "/";
+};
