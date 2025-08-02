7bb35e9031917533da59997e9c5728e59aef95c6
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-02 12:08:39 +0900

    Time: 0 ms (100%), Space: 52.3 MB (98.5%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,18 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
+    let colorsCount = [0, 0, 0];
+    for (num of nums) {
+        colorsCount[num]++;
+    }
     let i = 0;
-    while (i < nums.length - 1) {
-        let num = nums[i];
-        let minNum = num;
-        let minIndex = i;
-        for (let n = i + 1; n < nums.length; n++) {
-            if (minNum >= nums[n]) {
-                minNum = nums[n];
-                minIndex = n;
-            }
-        }
-       [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
-       i++;
+    let countIndex = 0;
+    while (i < nums.length) {
+        if (colorsCount[countIndex] > 0) {
+            nums[i] = countIndex;
+            colorsCount[countIndex]--;
+            i++;
+        } else countIndex++;
     }
 };
