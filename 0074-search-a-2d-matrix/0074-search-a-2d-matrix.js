cb2aac74592773f7f3971c296b184e1a4981c0c5
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-02 11:38:10 +0900

    Time: 0 ms (100%), Space: 54.1 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -10,32 +10,42 @@ var searchMatrix = function(matrix, target) {
     let n = matrix[0].length - 1;
     
     let left = 0;
-    let mid = Math.floor((left + m) / 2);
-    while (left < m) {
-        console.log(mid, left,  m);
-
+    let mid;
+    while (left <= m) {
+        mid = Math.floor((left + m) / 2);
         if (matrix[mid][0] > target) {
-            m = mid;
+            m = mid - 1;
         } else if (matrix[mid][0] < target) {
+            if (mid === matrix.length - 1) break;
             if (matrix[mid + 1][0] === target) return true;
             if (matrix[mid + 1][0] > target) break;
             left = mid + 1;
-        } 
-        mid = Math.floor((left + m) / 2);
-        console.log(mid, left,  m);
-        if (matrix[mid][0] === target) return true;        
+        } else return true;
     }
     left = 0;
     m = mid;
-    mid = Math.floor(n / 2);
-    while (left < n) {
+    while (left <= n) {
+        mid = Math.floor((left + n) / 2);
         if (matrix[m][mid] > target) {
             n = mid - 1;
         } else if (matrix[m][mid] < target) {
             left = mid + 1;
         } else return true;
-        mid = Math.floor((left + n) / 2);
-        if (matrix[m][mid] === target) return true;        
     }
     return false;
+
+// gpt - m * n 배열로 한번에 이진탐색
+    // let m = matrix.length, n = matrix[0].length;
+    // let left = 0, right = m * n - 1;
+
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2);
+    //     let val = matrix[Math.floor(mid / n)][mid % n];
+
+    //     if (val === target) return true;
+    //     else if (val < target) left = mid + 1;
+    //     else right = mid - 1;
+    // }
+
+    // return false;
 };
