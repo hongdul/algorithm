5f4f2a504d77ac97de4eb2615c1d7c55aeb70c06
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-02 20:55:39 +0900

    Time: 54 ms (84.23%), Space: 127.3 MB (94.91%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -5,20 +5,20 @@
  */
 var combine = function(n, k) {
     let answer = [];
-    for (let l = 1; l <= n - k + 1; l++) {
-        btr([l], k - 1);
-    }
-
+    
+    btr([], 1);
     return answer;
 
-    function btr(numArr, k) {
-        if (k === 0) {
-            answer.push(numArr);
+    function btr(numArr, start) {
+        if (numArr.length === k) {
+            answer.push([...numArr]);
             return;
         }
 
-        for (let m = numArr[numArr.length - 1] + 1; m <= n; m++) {
-            btr([...numArr, m], k - 1);
+        for (let m = start; m <= n; m++) {
+            numArr.push(m);
+            btr(numArr, m + 1);
+            numArr.pop();
         }
     }
 };
