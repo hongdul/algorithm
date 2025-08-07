1873500c26f3c0ecf29181763ee5d3e3e42017f6
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-07 14:24:38 +0900

    Time: 0 ms (100%), Space: 55.8 MB (29.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,19 +6,18 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let slicedNum1 = nums1.slice(0, m);
-    let index1 = 0;
-    let index2 = 0;
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let insertPos = m + n - 1;
 
-    while (index1 < m || index2 < n) {
-        num1 = index1 >= m ? Infinity : slicedNum1[index1];
-        num2 = index2 >= n ? Infinity : nums2[index2];
-        if (num1 < num2) {
-            nums1[index1 + index2] = slicedNum1[index1];
-            index1++;
+    while (p2 >= 0) {
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[insertPos] = nums1[p1];
+            p1--;
         } else {
-            nums1[index1 + index2] = nums2[index2];
-            index2++;
+            nums1[insertPos] = nums2[p2];
+            p2--;
         }
+        insertPos--;
     }
 };
