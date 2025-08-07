fc6c6cd84f1e2b2bc9c89d3878d28811a300bba8
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-07 16:38:48 +0900

    Time: 0 ms (100%), Space: 59.5 MB (39%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -3,28 +3,19 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    let answer = [];
+    const res = [];
     nums.sort((a, b) => a - b);
 
-    for(let i = 0; i <= nums.length; i++) {
-        btr([], 0, i);
-    }
-
-    return answer;
-
-    function btr(subset, index, length) {
-        if (subset.length === length) {
-            answer.push([...subset]);
-            return;
-        }
-        for (let i = index; i < nums.length; i++) {
-            while (i > index && nums[i] === nums[i - 1]) {
-                i++;
-                if (i >= nums.length) return;
-            }
-            subset.push(nums[i]);
-            btr(subset, i + 1, length);
-            subset.pop();
+    function dfs(start, path) {
+        res.push([...path]);
+        for (let i = start; i < nums.length; i++) {
+            if (i > start && nums[i] === nums[i - 1]) continue;
+            path.push(nums[i]);
+            dfs(i + 1, path);
+            path.pop();
         }
     }
+
+    dfs(0, []);
+    return res;
 };
