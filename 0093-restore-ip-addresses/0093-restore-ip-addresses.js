e7110aa4e719a8acd8a9d49a27df09d24b77a741
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-14 11:50:55 +0900

    Time: 3 ms (44.55%), Space: 53.9 MB (87.79%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.js
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.js
@@ -6,22 +6,21 @@ var restoreIpAddresses = function(s) {
     // 1. '.' 로 구분된 (0 ~ 255) 범위의 숫자 4개
     // 2. 0 은 가능하지만 '01'은 안됨(leading zeros)
     let answer = [];
+    let minLen = s.length <= 10 ? 1 : s.length % 9;
     dfs(0, []);
     return answer;
 
     function dfs(idx, numArr) {
-        if (numArr.length > 4) return;
+        if (numArr.length > 4 || (s.length - idx) > 3 * (4 - numArr.length)) return;
         if (idx === s.length && numArr.length === 4) {
             answer.push(numArr.join('.'));
             return;
         }
         
-        for (let i = 1; i <= 3; i++) {
+        for (let i = minLen; i <= 3; i++) {
+            if (idx + i > s.length) continue;
             let slicedNum = s.slice(idx, idx + i);
-            if (Number(slicedNum) > 255 
-                || (slicedNum.length > 1 && slicedNum[0] === '0')
-                || idx + i > s.length
-            ) {
+            if (Number(slicedNum) > 255 || (slicedNum.length > 1 && slicedNum[0] === '0')) {
                 continue;
             }
             numArr.push(slicedNum);
