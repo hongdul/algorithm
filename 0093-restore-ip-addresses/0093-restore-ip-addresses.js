ffe1c1c49c64415b18fabf8ac29be4e92fbc4d27
hongdul <48382951+hongdul@users.noreply.github.com>
2025-08-14 12:05:00 +0900

    Time: 1 ms (87.13%), Space: 54 MB (84.49%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.js
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.js
@@ -6,23 +6,29 @@ var restoreIpAddresses = function(s) {
     // 1. '.' 로 구분된 (0 ~ 255) 범위의 숫자 4개
     // 2. 0 은 가능하지만 '01'은 안됨(leading zeros)
     let answer = [];
-    let minLen = s.length <= 10 ? 1 : s.length % 9;
     dfs(0, []);
     return answer;
 
     function dfs(idx, numArr) {
         if (numArr.length > 4 || (s.length - idx) > 3 * (4 - numArr.length)) return;
+
+        const remain = 4 - numArr.length;
+        const rest = s.length - idx;
+        if (rest < remain) return;
+
         if (idx === s.length && numArr.length === 4) {
             answer.push(numArr.join('.'));
             return;
         }
-        
-        for (let i = minLen; i <= 3; i++) {
-            if (idx + i > s.length) continue;
-            let slicedNum = s.slice(idx, idx + i);
-            if (Number(slicedNum) > 255 || (slicedNum.length > 1 && slicedNum[0] === '0')) {
-                continue;
-            }
+        if (numArr.length === 4) return;
+
+        const maxLen = (s[idx] === '0') ? 1 : 3;
+
+        for (let i = 1; i <= maxLen && idx + i <= s.length; i++) {
+            const slicedNum = s.slice(idx, idx + i);
+            
+            if (Number(slicedNum) > 255) break;
+
             numArr.push(slicedNum);
             dfs(idx + i, numArr);
             numArr.pop();
